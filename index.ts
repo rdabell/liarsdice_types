7fa23fa5b0bd94678a2a77f439d3a091b0568039
rdabell <r.mark.dabell@gmail.com>
2017-11-27 20:09:54 -0700

    implement barrel file

---
+++ index.ts
@@ -0,0 +1,7 @@
+export * from './src/types/dice';
+export * from './src/types/bid';
+export * from './src/types/player';
+export * from './src/types/game';
+export * from './src/types/chat';
+export * from './src/types/message';
+export * from './src/types/turn';
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-import {Dice} from './types/dice';
-import {Bid} from './types/bid';
